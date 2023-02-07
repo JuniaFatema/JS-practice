ab5e3edd8014eadcc57c2b484d0877fa73964a32
JuniaFatema <junia01123@gmail.com>
2023-02-07 09:21:46 -0500

    Box-Square asteric problem

---
+++ Box-1.js
@@ -0,0 +1,20 @@
+let input = `3
+1
+2
+5`;
+let lines = input.split('\n');
+
+const T = Number(lines.shift());
+for(let i = 0; i < T; i++) {
+  let n = Number(lines[i])
+
+  let string = "";
+
+  for(let x = 0; x < Number(lines[i]); x++) { 
+    for(let j = 0; j < Number(lines[i]); j++) { 
+      string = string + "*";
+    }
+    string = string +  "\n"
+  }
+  console.log(string)
+}
