1d655ed627ef8f54428959948c959ed7ab51be2e
JuniaFatema <junia01123@gmail.com>
2023-02-07 09:43:44 -0500

    factors

---
+++ fgf.js
@@ -0,0 +1,19 @@
+let input = `3
+1
+2
+5`;
+let lines = input.split('\n');
+
+const T = Number(lines.shift());
+for(let i = 0; i < T; i++) {
+  
+  let string = "";
+
+  for(let x = 0; x < Number(lines[i]); x++) { 
+    for(let j = 0; j < Number(lines[i]); j++) { 
+      string = string + "*";
+    }
+    string = string +  "\n"
+  }
+  console.log(string)
+}
